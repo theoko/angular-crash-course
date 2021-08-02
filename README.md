2a05b79018fb91000c9ce3ae31c9341706282d9c
theoko <konstantopoulostheodore@gmail.com>
2021-08-02 08:42:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-p9droi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-p9droi)
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
